e3b16c2cc5c441d5f98929896f4827765c669223
Pawel Jaskula <pj.jaskula@gmail.com>
2017-05-13 14:51:51 +0200

    Program
    This is Program class with main

---
+++ Program.java
@@ -0,0 +1,12 @@
+package Paint;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame f1 = new Frame();
+		f1.setVisible(true);
+		f1.setDefaultCloseOperation(3);
+	}
+
+}
